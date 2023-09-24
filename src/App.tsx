9c8a754e24aa509ac7fcd8acfb9501c4084b3a38
Abisola Adebiyi <abisola.ca@gmail.com>
2023-09-23 20:26:23 -0700

    create auth functionality with firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
 import React, { useEffect } from "react";
-import {
-  Route,
-  Routes,
-  useLocation,
-  useNavigate,
-} from "react-router-dom";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import Auth from "./pages/Auth";
 import Board from "./pages/Board";
 import Boards from "./pages/Boards";
+import { onAuthStateChanged } from "@firebase/auth";
+import { auth } from "./firebase";
 
 import { ROUTES } from "./utils/constants/routes";
 
 const App = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const location = useLocation();
 
   useEffect(() => {
-    if(location.pathname === '/') navigate(ROUTES.BOARDS)
-  }, [location, navigate]);
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        navigate(ROUTES.BOARDS);
+      } else {
+        if ([ROUTES.LOGIN, ROUTES.SIGNUP].includes(location.pathname)) return;
+        navigate(ROUTES.LOGIN);
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    if (location.pathname === "/") navigate(ROUTES.BOARDS);
+  }, []);
 
   return (
     <div className="App h-full">
---
+++ src/firebase.ts
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore";
+import '@firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const auth = getAuth()
+export const db = getFirestore()
+export const database = getDatabase()
